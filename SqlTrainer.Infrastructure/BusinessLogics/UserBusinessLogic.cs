63792c2b38b9226c6dd5f89048cbb8df79f9c3fa
Vadym Naumenko <73651247+vda132@users.noreply.github.com>
2022-11-26 12:37:33 +0200

    implement password hasher

--- SqlTrainer.Infrastructure/BusinessLogics/UserBusinessLogic.cs
+++ SqlTrainer.Infrastructure/BusinessLogics/UserBusinessLogic.cs
@@ -1,5 +1,6 @@
 ﻿using OperationResults.Generic;
 using SqlTrainer.Application.BusinessLogics;
+using SqlTrainer.Application.PasswordHasher;
 using SqlTrainer.Application.Repositories;
 using SqlTrainer.Domain.Models;
 
@@ -8,12 +9,16 @@ namespace SqlTrainer.Infrastructure.BusinessLogics;
 public sealed class UserBusinessLogic : IUserBusinessLogic
 {
     private readonly IUserRepository repository;
+    private readonly IPasswordHasher passwordHasher;
 
-    public UserBusinessLogic(IUserRepository repository)
+    public UserBusinessLogic(IUserRepository repository, IPasswordHasher passwordHasher)
     {
         this.repository = repository;
+        this.passwordHasher = passwordHasher;
     }
 
+
+    // ToDo: Hash password before adding new user to db
 
     public async Task<IOperationResult<IReadOnlyCollection<User>>> GetByLoginAsync(string login)
     {
---
+++ SqlTrainer.Infrastructure/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,26 @@
+﻿using OperationResults.Generic;
+using OperationResults.Services;
+using OperationResults.Services.Parameters;
+using SqlTrainer.Application.PasswordHasher;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SqlTrainer.Infrastructure.PasswordHasher;
+
+public sealed class PasswordHasher : IPasswordHasher
+{
+    public IOperationResult<string> HashPassword(string password)
+    {
+        var param = ParamsFactory.CreateSimpleWithResult(Hash, password);
+
+        return OperationService.DoOperationWithResult(param);
+    }
+
+    private string Hash(string password)
+    {
+        var md5 = MD5.Create();
+        var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        return Convert.ToBase64String(hash);
+    }
+}
